89b4e3f9db5b960d4d71cb2eddc38e42ff5f7ba4
matkrawc <mateuszkr@student.agh.edu.pl>
2023-05-27 15:21:41 +0200

    add sample vars

---
+++ src/main/java/org/example/reports/Report_1.java
@@ -0,0 +1,15 @@
+package org.example.reports;
+
+import java.util.List;
+
+public class Report_1 {
+
+    private String project;
+    private int hours;
+
+
+    public static void countingHoursPerProject(List listOfAll) {
+//        listOfAll.
+
+    }
+}
