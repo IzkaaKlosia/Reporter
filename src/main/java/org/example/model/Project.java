9ce22b425a145c08a2a2bfc63c1824fb8033369a
jmakowie <jakub.m.makowiecki@gmail.com>
2023-05-27 13:54:37 +0200

    create model

--- src/main/java/org/example/model/Project.java
+++ src/main/java/org/example/model/Project.java
@@ -1,31 +1,9 @@
 package org.example.model;
 
-import java.util.Objects;
-
+import lombok.Builder;
+import lombok.Data;
+@Data
+@Builder
 public class Project {
     private final String name;
-
-    public Project(String name) {
-        this.name = name;
-    }
-
-    @Override
-    public String toString() {
-        return "Project{" +
-                "name='" + name + '\'' +
-                '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Project project = (Project) o;
-        return Objects.equals(name, project.name);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name);
-    }
 }
--- src/main/java/org/example/model/Task.java
+++ src/main/java/org/example/model/Task.java
@@ -1,46 +1,19 @@
 package org.example.model;
 
+import lombok.Builder;
+import lombok.Data;
+
 import java.time.LocalDate;
 import java.util.List;
-import java.util.Objects;
-
+@Data
+@Builder
 public class Task {
     private final String name;
     private final LocalDate date;
     private final Double timeAmount;
     private List<Project> projects;
 
-    public Task(String name, LocalDate localDate, Double timeAmount, List<Project> projects) {
-        this.name = name;
-        this.date = localDate;
-        this.timeAmount = timeAmount;
-        this.projects = projects;
-    }
-
     public void addProject(Project project){
         this.projects.add(project);
-    }
-
-    @Override
-    public String toString() {
-        return "Task{" +
-                "name='" + name + '\'' +
-                ", localDate=" + date +
-                ", timeAmount=" + timeAmount +
-                ", projects=" + projects +
-                '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Task task = (Task) o;
-        return Objects.equals(name, task.name) && Objects.equals(date, task.date) && Objects.equals(timeAmount, task.timeAmount) && Objects.equals(projects, task.projects);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name, date, timeAmount, projects);
     }
 }
